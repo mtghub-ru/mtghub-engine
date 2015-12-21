fd42886a4df25e78ad18aeec06719a3869a1c63b
Glebov Boris <avatar29A@gmail.com>
2015-12-21 10:05:54 +0500

    add skeleton project's files.
    add example magic game machine language.

---
+++ test/mgml-example.lisp
@@ -0,0 +1,78 @@
+(make-game :type 'duel
+           :players '(victim, hunter98))
+
+(make-player *player1* :health 20 :nickname: 'victim)
+(make-player *player2* :health 20 :nickname: 'hunter98)
+
+(make-deck *player1*
+           (card "Фантом Джейса")
+           (card "Маг Гильдии")
+           (card "Island")
+           (card ...))
+
+(make-deck *player2*
+           (card "Сфера задержания")
+           (card "Храм Таинств")
+           (card ...))
+
+;; Take seven card for *player1*
+(put (hand *player1*) (card "Водная Могила"))
+(put (hand *player1*) (card "Убийство"))
+(put (hand *player1*) (card "Маг Гильдии из Мантии Сумрака"))
+(put (hand *player1*) (card "Растущий беспорядок"))
+(put (hand *player1*) (card "Растущий беспорядок"))
+(put (hand *player1*) (card "Последняя Цена"))
+(put (hand *player1*) (card "Фантом Джейса"))
+
+;; Take seven card for *player2*
+(put (hand *player2*) (card "Растворение"))
+(put (hand *player2*) (card "Лес"))
+(put (hand *player2*) (card "Киора, Разбивающаяся Волна"))
+(put (hand *player2*) (card "Лесная Кариатида"))
+(put (hand *player2*) (card "Остров"))
+(put (hand *player2*) (card "Высочайший Вердикт"))
+(put (hand *player2*) (card "Церковный Сад"))
+
+;; turn 0
+(turn *player1*
+      (main-phase
+       (cast-land (card "Водная Могила")
+                  (chose 1)))
+      (combat-phase
+       (beginning-combat-step (pass))
+       (declare-attackers-step (pass))
+       (declare-blockers-step (pass))
+       (combat-damage-step (pass))
+       (end-combat-step) (pass))
+      (main-phase (pass))
+      (ending-phase
+       (end-step (pass))
+       (cleanup-step (pass))))
+
+;; turn 1
+(turn *player2*
+      (beginning-phase
+       (untap-step )
+       (upkeep-step)
+       (draw-step
+        (put (hand *active-player*) (card "Резвая Бегунья Круфикса"))))
+      (main-phase
+       (cast-land (get (hand *active-player*) (card "Церковный Сад"))
+                  (chose 1)))
+      (pass))
+
+;; turn 2
+(turn *player1*
+      (beginning-phase
+       (untap-step
+        ;; When spell was resolved, then would created new permanent with unique ID.
+        (untap (permanent #"Водная Могила [01]")))
+       (upkeep-step (pass))
+       (draw-step
+        (put (hand *active-player*) (card "Последняя Цена"))))
+      (main-phase
+       ;; Activate ability. Common: (active (ref on permanent) #AbilityNumber)
+       (active (permanent #"Водная Могила [01]") (chose 0))
+       ))
+
+;; turn 3
